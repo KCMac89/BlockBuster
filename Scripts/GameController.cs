faf6bc14eacf7b1837e8bb7f314cbabab5378955
KCMac89 <74109248+KCMac89@users.noreply.github.com>
2021-02-13 18:24:43 -0600

    Add files via upload

---
+++ Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+
+    public EnemyProducer enemyProducer;
+    public GameObject playerPrefab;
+    public Text GameOverText;
+
+    void Start()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        player.onPlayerDeath += onPlayerDeath;
+    }
+
+    void onPlayerDeath(Player player)
+    {
+        enemyProducer.SpawnEnemies(false);
+        Destroy(player.gameObject);
+        GameOverText.text = ("GAME OVER");
+        Invoke("restartGame", 10);
+    }
+
+    void restartGame()
+    {
+        GameOverText.text = ("");
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+
+        var playerObject = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), Quaternion.identity) as GameObject;
+        var cameraRig = Camera.main.GetComponent<CameraRig>();
+        cameraRig.target = playerObject;
+        enemyProducer.SpawnEnemies(true);
+        playerObject.GetComponent<Player>().onPlayerDeath += onPlayerDeath;
+    }
+
+}
+
---
+++ Scripts/GameSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpeed : MonoBehaviour
+{
+    public Slider slider;
+    public Text sliderText;
+   // public float gameSpeed;
+   // public float maxSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // slider.maxValue = maxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderText.text = slider.value.ToString("f");
+        slider.onValueChanged.AddListener(delegate { ChangeGameSpeed(); });
+        //slider.value = Time.timeScale;
+    }
+
+    public void ChangeGameSpeed()
+    {
+        Time.timeScale = slider.value;
+    }
+   
+}
